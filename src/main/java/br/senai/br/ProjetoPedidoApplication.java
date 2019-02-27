b002ab40ea0775af069ca6426b439ef5827e02d7
Matheus Knabben <nazariotancredo@gmail.com>
2019-02-26 22:48:38 -0300

    Qualquer comentario

--- src/main/java/br/senai/br/ProjetoPedidoApplication.java
+++ src/main/java/br/senai/br/ProjetoPedidoApplication.java
@@ -1,4 +1,4 @@
-package ProjetoPedido;
+package br.senai.br;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/br/senai/br/domain/Categoria.java
@@ -0,0 +1,5 @@
+package br.senai.br.domain;
+
+public class Categoria {
+
+}
